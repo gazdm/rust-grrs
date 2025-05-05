7d646c5ae34d2c68edb73685894da9fe70f9b1cb
gazdm <adm.a90@tutanota.com>
2025-05-04 21:37:41 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no path given");
+
+    println!("pattern: {:?}, path: {:?}", pattern, path)
+}
