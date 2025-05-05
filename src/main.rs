be8d446ff7e31532c12b09b08e0dc714748028a7
gazdm <adm.a90@tutanota.com>
2025-05-04 22:34:15 -0600

    arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
+    /// The path to the file to read
     path: std::path::PathBuf,
 }
 
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
 
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-
-    println!("pattern: {:?}, path: {:?}", pattern, path)
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
 }
